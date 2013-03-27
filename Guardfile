f55c455df503f5334308d917de3441bbdcc12dbe
Yasuhito Takamiya <yasuhito@gmail.com>
2013-03-27 17:33:21 +0900

    Enable spork + guard

--- Guardfile
+++ Guardfile
@@ -1,7 +1,22 @@
-# A sample Guardfile
 # More info at https://github.com/guard/guard#readme
 
-guard 'cucumber' do
+
+guard 'spork' do
+  watch('Gemfile')
+  watch('Gemfile.lock')
+  watch('spec/spec_helper.rb') { :rspec }
+  watch(%r{features/support/}) { :cucumber }
+end
+
+
+guard 'rspec', :cli => "--drb" do
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch('spec/spec_helper.rb')  { "spec" }
+end
+
+
+guard 'cucumber', :cli => "--drb" do
   watch(%r{^features/.+\.feature$})
   watch(%r{^features/support/.+$})          { 'features' }
   watch(%r{^features/step_definitions/(.+)_steps\.rb$}) { |m| Dir[File.join("**/#{m[1]}.feature")][0] || 'features' }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,53 @@
-require "coveralls"
-Coveralls.wear!
+#
+# Copyright (C) 2013 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
-require "aruba/cucumber"
+
+require "rubygems"
+require "spork"
+#uncomment the following line to use spork with the debugger
+#require "spork/ext/ruby-debug"
+
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  if not ENV[ "DRB" ]
+    require "coveralls"
+    Coveralls.wear!
+  end
+
+  require "aruba/cucumber"
+end
+
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+  if ENV[ "DRB" ]
+    require "coveralls"
+    Coveralls.wear!
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
