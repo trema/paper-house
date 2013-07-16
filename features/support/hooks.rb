95027163fdaf5bb4761b051ea8a09f667761e55f
Yasuhito Takamiya <yasuhito@gmail.com>
2013-07-17 08:36:39 +0900

    Set env variable to suppress dragonegg warnings.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,12 @@
 Before do
   ENV[ "CC" ] = nil
+  ENV[ "dragonegg_disable_version_check" ] = "1"
   @aruba_timeout_seconds = 5
 end
 
 
 After do
   ENV[ "CC" ] = nil
+  ENV[ "dragonegg_disable_version_check" ] = nil
   Rake::Task.clear
 end
