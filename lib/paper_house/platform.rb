a89ec6ee7f347e03fabaeea93454a216c3d3929e
Yasuhito Takamiya <yasuhito@gmail.com>
2014-03-27 12:14:41 +0900

    Freeze some values for safety.

--- lib/paper_house/platform.rb
+++ lib/paper_house/platform.rb
@@ -11,13 +11,13 @@ module PaperHouse
     MAC = (/darwin|mac os/ =~ CONFIG['host_os'])
 
     # File extension of C extensions.
-    SHARED_EXT = MAC ? '.bundle' : '.so'
+    SHARED_EXT = (MAC ? '.bundle' : '.so').freeze
 
     # CC options for compiling shared libraries.
-    LDSHARED = MAC ? '-dynamic -bundle' : '-shared'
+    LDSHARED = (MAC ? '-dynamic -bundle' : '-shared').freeze
 
     # CC option for setting soname.
-    SONAME_OPTION = MAC ? '-install_name' : '-soname'
+    SONAME_OPTION = (MAC ? '-install_name' : '-soname').freeze
 
     # Include directories for compiling C extensions.
     RUBY_INCLUDES =
@@ -25,13 +25,13 @@ module PaperHouse
         File.join(CONFIG['rubyhdrdir'], CONFIG['arch']),
         File.join(CONFIG['rubyhdrdir'], 'ruby/backward'),
         CONFIG['rubyhdrdir']
-      ]
+      ].freeze
 
     # Library directories for compiling C extensions.
-    RUBY_LIBDIR = CONFIG['libdir']
+    RUBY_LIBDIR = CONFIG['libdir'].freeze
 
     def self.name
-      MAC ? 'mac' : 'linux'
+      (MAC ? 'mac' : 'linux').freeze
     end
   end
 end
